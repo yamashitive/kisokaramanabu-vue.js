e47b09b21421bf5061be5501774a2daeacca6135
yamashitive <mail@nmpd.jp>
2020-05-27 23:32:34 +0900

    P41までの作業が終了

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 var app = new Vue({
   el: "#app",
+  data: {
+    message: "Hello Vue.js!",
+    list: ["りんご", "ばなな", "いちご"],
+    count: 1,
+    show: true,
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target);
+    },
+  },
 });
+
+console.log(app.message);
